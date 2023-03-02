06bd3fdd9bb339fc682f18f246cf30379e72d71c
viktoriya <viktoriyam356@gmail.com>
2023-03-02 17:15:55 +0300

    задача 19

---
+++ 019/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("your number = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N % 10 == N / 10000)
+{
+    if (((N % 100) - (N / 10000)) / 10 == (N / 1000) - ((N % 10) * 10))
+    {
+        Console.WriteLine("yes ^_^");
+    }
+}
+else
+{
+    Console.WriteLine("no...");
+}
+
+
+/*Console.Write("your number = ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int result = (N / 1000) - ((N % 10) * 10);
+Console.WriteLine(result);*/
